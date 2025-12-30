f9c2909f124c53f2f5cc43c0654854eeb3216d73
Habibe Çınar <habibecinar07@gmail.com>
2025-12-30 11:39:14 +0300

    setState yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import projects from "./data/mockData";
+import initialProjects from "./data/mockData";
+import { useState } from "react";
 
 function App() {
+    const [projects, setProjects] = useState(initialProjects);
+    const addProject = () => {
+        const newProject = {
+             id: Date.now(),
+    name: `New Project`,
+    createdAt: new Date().toISOString().split("T")[0],
+    tasks: [],
+        };
+        setProjects((prev) => [...prev, newProject]);
+    };
     return (
         <div>   
             <h1>Project List</h1>
             <ul>
-                {projects.map(project => (
+                {projects.map((project) => (
                     <li key={project.id}>
                         <h2>{project.name}</h2>
                     </li>
                 ))}
             </ul>
+            <button onClick={addProject}>Add Project</button>
         </div>
+        
     );
 }
 export default App;
